c6b8fc3c898477c5b44a3b372dfdd8d3c47d2567
bqtien <bqtien.hd@gmail.com>
2022-12-20 10:40:04 +0700

    create OVL token

---
+++ ovl.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract Krabots is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
+    
+    using SafeMath for uint;
+
+    uint256 private totalTokens;
+
+    constructor() ERC20("Overleague", "OVL") {
+        totalTokens = 100000000 * 10** decimals(); // 100M token
+        _mint(owner(), totalTokens);  
+    }
+
+    function snapshot() external onlyOwner {
+        _snapshot();
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function getBurnedAmountTotal() external view returns (uint256 _amount) {
+        return totalTokens.sub(totalSupply());
+    }
+}
